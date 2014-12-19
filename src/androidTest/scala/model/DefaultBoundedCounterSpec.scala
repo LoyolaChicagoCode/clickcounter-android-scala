5605c5c29ef6d83737c29074a86e7efdf7c3a5f7
Konstantin Läufer <laufer@cs.luc.edu>
2014-12-18 22:34:10 -0600

    updated Android sbt plugin version and moved tests to src/test
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/androidTest/scala/model/DefaultBoundedCounterSpec.scala
+++
@@ -1,6 +0,0 @@
-package edu.luc.etl.cs313.scala.clickcounter.model
-
-/** A concrete testcase subclass for StatelessBoundedCounter. */
-class DefaultBoundedCounterSpec extends AbstractBoundedCounterSpec {
-  override def fixture() = new DefaultBoundedCounter(0, 10)
-}
